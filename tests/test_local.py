ed8ff015e3d9389243bcf1ddc58ef1c8bd587fbf
Mateus Furquim <furquim@devgrid.co.uk>
2022-11-10 23:57:47 -0300

    Is it deployed on heroku if github pipeline fails?

--- tests/test_local.py
+++ tests/test_local.py
@@ -5,4 +5,4 @@ def test_root(client):
     """Test if root is returning the expected greeting."""
     r = client.get("/")
     assert r.status_code == 200
-    assert r.json() == {"greeting": "Hello World!"}
+    assert r.json() == {"greeting": "Bye World!"}
