bd5642db1b153ca1182306529b8a1aeb1f83e8ac
Mateus Furquim <furquim.axs@gmail.com>
2022-11-30 13:38:05 -0300

    Move source files to src and test file to tests

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"Larissa": "Aceita casar comigo? ❤️"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
--- tests/test_local.py
+++ tests/test_local.py
@@ -1,11 +1,7 @@
 """Basic API testing on FastAPI."""
-from fastapi.testclient import TestClient
-from main import app
-
-client = TestClient(app)
 
 
-def test_root():
+def test_root(client):
     """Test if root is returning the expected greeting."""
     r = client.get("/")
     assert r.status_code == 200
