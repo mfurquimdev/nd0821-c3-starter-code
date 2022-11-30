503b209f72525cc2376a447349e40cb4736c2cb2
Mateus Furquim <furquim.axs@gmail.com>
2022-11-30 13:38:07 -0300

    Add heroku support for DVC

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 """FastAPI app definition"""
+import os
+
 import uvicorn
 from fastapi import FastAPI
-
 from src.root.view import router as root_router
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 app = FastAPI()
 
