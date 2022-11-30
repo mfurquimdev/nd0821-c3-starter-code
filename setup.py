50804a9b71b521c9e78c99cfe35e4d5dc33a9679
Mateus Furquim <furquim.axs@gmail.com>
2022-11-30 13:38:07 -0300

    Move setup and sanitycheck to root

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="ml_model_fastpi",
+    version="0.0.1",
+    description="API to serve census model.",
+    author="Mateus Furquim <mateus@mfurquim.dev>",
+)
