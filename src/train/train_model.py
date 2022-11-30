372e7ec1d56a0114d00b3a8964b76507465ac89a
Mateus Furquim <furquim.axs@gmail.com>
2022-11-30 19:24:44 -0300

    Remove code to save X and y data set

--- src/train/train_model.py
+++ src/train/train_model.py
@@ -18,12 +18,6 @@ data_dir = "data"
 data_filepath = Path(data_dir, data_filename)
 data = pd.read_csv(data_filepath)
 
-test_dir = "tests"
-data_dir = "data"
-
-
-n = 5
-
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
@@ -45,14 +39,6 @@ X_train, y_train, encoder, lb = process_data(
     training=True,
 )
 
-filename = "X_train.csv"
-filepath = Path(test_dir, data_dir, filename)
-np.savetxt(filepath, X_train[:n], delimiter=",")
-
-filename = "y_train.csv"
-filepath = Path(test_dir, data_dir, filename)
-np.savetxt(filepath, y_train[:n], delimiter=",")
-
 # Proces the test data with the process_data function.
 X_test, y_test, _, _ = process_data(
     test,
@@ -63,24 +49,12 @@ X_test, y_test, _, _ = process_data(
     lb=lb,
 )
 
-filename = "X_test.csv"
-filepath = Path(test_dir, data_dir, filename)
-np.savetxt(filepath, X_test[:n], delimiter=",")
-
-filename = "y_test.csv"
-filepath = Path(test_dir, data_dir, filename)
-np.savetxt(filepath, y_test[:n], delimiter=",")
-
 # Train and save a model.
 model = train_model(X_train, y_train)
 
 save_model(model, encoder, lb)
 
 y_pred = inference(model, X_test)
-
-filename = "y_pred.csv"
-filepath = Path(test_dir, data_dir, filename)
-np.savetxt(filepath, y_pred[-n:], delimiter=",")
 
 precision, recall, fbeta = compute_model_metrics(y_test, y_pred)
 print(f"precision: {precision}")
