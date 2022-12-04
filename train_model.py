459848348296a44f6e8df63bca4c3bb0d42365fb
Mateus Furquim <furquim.axs@gmail.com>
2022-12-03 21:59:44 -0300

    Move train script to inside a main function

--- train_model.py
+++ train_model.py
@@ -12,58 +12,65 @@ from src.ml.model import inference
 from src.ml.model import save_model
 from src.ml.model import train_model
 
-data_filename = "census.csv"
-data_dir = "data"
-data_filepath = Path(data_dir, data_filename)
-data = pd.read_csv(data_filepath)
 
-train, test = train_test_split(data, test_size=0.20)
+def main():
+    data_filename = "census.csv"
+    data_dir = "data"
+    data_filepath = Path(data_dir, data_filename)
+    data = pd.read_csv(data_filepath)
 
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
+    train, test = train_test_split(data, test_size=0.20)
+
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    X_train, y_train, encoder, lb = process_data(
+        train,
+        categorical_features=cat_features,
+        label="salary",
+        training=True,
+    )
+
+    X_test, y_test, _, _ = process_data(
+        test,
+        categorical_features=cat_features,
+        label="salary",
+        training=False,
+        encoder=encoder,
+        lb=lb,
+    )
+
+    model = train_model(X_train, y_train)
+
+    save_model(model, encoder, lb)
+
+    y_pred = inference(model, X_test)
+
+    precision, recall, fbeta = compute_model_metrics(y_test, y_pred)
+
+    category_dict = compute_metrics_on_slices(test, model, encoder, lb, cat_features)
+
+    with open("slice_output.txt", "w") as f:
+        f.write("All data set metrics\n")
+        f.write("precision,recall,fbeta\n")
+        f.write(f"{round(precision,3)},{round(recall,3)},{round(fbeta,3)}\n")
+
+        for cat_name, slice_dict in category_dict.items():
+            f.write(f"\nMetrics for category: {cat_name}\n")
+            f.write("slice_value,precision,recall,fbeta\n")
+            for slice_value, metrics in slice_dict.items():
+                f.write(
+                    f"{slice_value},{round(metrics.precision,3)},{round(metrics.recall,3)},{round(metrics.fbeta,3)}\n"
+                )
 
 
-X_train, y_train, encoder, lb = process_data(
-    train,
-    categorical_features=cat_features,
-    label="salary",
-    training=True,
-)
-
-X_test, y_test, _, _ = process_data(
-    test,
-    categorical_features=cat_features,
-    label="salary",
-    training=False,
-    encoder=encoder,
-    lb=lb,
-)
-
-model = train_model(X_train, y_train)
-
-save_model(model, encoder, lb)
-
-y_pred = inference(model, X_test)
-
-precision, recall, fbeta = compute_model_metrics(y_test, y_pred)
-
-category_dict = compute_metrics_on_slices(test, model, encoder, lb, cat_features)
-
-with open("slice_output.txt", "w") as f:
-    f.write("All data set metrics\n")
-    f.write("precision,recall,fbeta\n")
-    f.write(f"{round(precision,3)},{round(recall,3)},{round(fbeta,3)}\n")
-
-    for cat_name, slice_dict in category_dict.items():
-        f.write(f"\nMetrics for category: {cat_name}\n")
-        f.write("slice_value,precision,recall,fbeta\n")
-        for slice_value, metrics in slice_dict.items():
-            f.write(f"{slice_value},{round(metrics.precision,3)},{round(metrics.recall,3)},{round(metrics.fbeta,3)}\n")
+if __name__ == "__main__":
+    main()
